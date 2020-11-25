e52f57ef4e9d051ed4c95f57184d224cbd987f1a
cyliong <liongtheprogrammer@gmail.com>
2020-11-25 17:38:36 +0800

    Remove unnecessary template logic

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,18 +40,14 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'No Items',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: null,
+        tooltip: 'Add',
         child: Icon(Icons.add),
       ),
     );
