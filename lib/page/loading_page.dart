8120efb52336e1dff5ffb93e6de20063c8bc8aa3
cyliong <liongtheprogrammer@gmail.com>
2020-11-28 21:04:10 +0800

    Set app bar for LoadingPage

--- lib/page/loading_page.dart
+++ lib/page/loading_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:items/constants.dart';
 
 class LoadingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(kAppTitle),
+      ),
       body: Center(
         child: CircularProgressIndicator(),
       ),
