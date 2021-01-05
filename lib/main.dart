6d8ddb0daf37b9726f3f66473b2f3cef6bc855c9
cyliong <liongtheprogrammer@gmail.com>
2021-01-05 23:33:14 +0800

    Implement FlutterFire initialization logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:items/constants.dart';
+import 'package:items/page/error_page.dart';
 import 'package:items/page/home_page.dart';
+import 'package:items/page/loading_page.dart';
 
-void main() => runApp(App());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
 
 class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return _buildMaterialApp(ErrorPage('${snapshot.error}'));
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return _buildMaterialApp(HomePage());
+          }
+          return _buildMaterialApp(LoadingPage());
+        });
+  }
+
+  MaterialApp _buildMaterialApp(Widget home) {
     return MaterialApp(
       title: kAppTitle,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: home,
       debugShowCheckedModeBanner: false,
     );
   }
