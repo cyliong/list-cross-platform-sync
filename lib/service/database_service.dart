4814233e55b83b314e9db622c6c3a428127a7eab
cyliong <liongtheprogrammer@gmail.com>
2021-01-26 22:13:52 +0800

    Implement a getter to retrieve items from remote database

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -1,9 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:items/model/list_item.dart';
 
 class DatabaseService {
   final _firestore = FirebaseFirestore.instance;
 
-  CollectionReference get items {
-    return _firestore.collection('items');
+  Future<List<ListItem>> get items async {
+    final querySnapshot = await _firestore.collection('items').get();
+    return querySnapshot.docs
+        .map((snapshot) => ListItem.fromMap(snapshot.data(), snapshot.id))
+        .toList();
   }
 }
