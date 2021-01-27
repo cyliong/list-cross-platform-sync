86fabd00591389e8b22398ef5898155f3eb290ac
cyliong <liongtheprogrammer@gmail.com>
2021-01-27 16:55:32 +0800

    Define a constant for the collection "items"

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -2,10 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:items/model/list_item.dart';
 
 class DatabaseService {
+  static const _itemsCollection = 'items';
+
   final _firestore = FirebaseFirestore.instance;
 
   Future<List<ListItem>> get items async {
-    final querySnapshot = await _firestore.collection('items').get();
+    final querySnapshot = await _firestore.collection(_itemsCollection).get();
     return querySnapshot.docs
         .map((snapshot) => ListItem.fromMap(snapshot.data(), snapshot.id))
         .toList();
