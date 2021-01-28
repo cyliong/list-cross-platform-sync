fc0dbdd8a70129ea93d47f406b4c8a8ca110565f
cyliong <liongtheprogrammer@gmail.com>
2021-01-28 18:30:26 +0800

    Create the ItemPage skeleton

---
+++ lib/page/item_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ItemPage extends StatefulWidget {
+  @override
+  _ItemPageState createState() => _ItemPageState();
+}
+
+class _ItemPageState extends State<ItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
