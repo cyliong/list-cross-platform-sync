52b96bc7732afbe1b72cf5698cda4e2a7ebf7c44
cyliong <liongtheprogrammer@gmail.com>
2021-02-09 22:18:07 +0800

    Add the created field to ListItem

--- lib/model/list_item.dart
+++ lib/model/list_item.dart
@@ -1,23 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ListItem {
   static const _titleField = 'title';
   static const createdField = 'created';
 
   String id;
   String title;
+  Timestamp created;
 
   ListItem({
     this.id,
     this.title,
+    this.created,
   });
 
   ListItem.fromMap(Map<String, dynamic> map, String id) {
     this.id = id;
     this.title = map[_titleField];
+    this.created = map[createdField];
   }
 
   Map<String, dynamic> toMap() {
     final map = <String, dynamic>{};
     map[_titleField] = title;
+    map[createdField] = created;
     return map;
   }
 }
