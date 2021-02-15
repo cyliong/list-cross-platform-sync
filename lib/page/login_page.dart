821f158c5afde45cbefa87f1fe8156e0b49a88d9
cyliong <liongtheprogrammer@gmail.com>
2021-02-15 23:32:12 +0800

    Set app bar for LoginPage

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -8,6 +8,10 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log In'),
+      ),
+    );
   }
 }
