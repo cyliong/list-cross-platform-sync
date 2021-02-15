b4b439db32078bb7978b313fd219468054e79882
cyliong <liongtheprogrammer@gmail.com>
2021-02-15 23:35:34 +0800

    Add the signOut() method to AuthenticationService

--- lib/service/authentication_service.dart
+++ lib/service/authentication_service.dart
@@ -2,4 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenticationService {
   final _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> signOut() => _firebaseAuth.signOut();
 }
