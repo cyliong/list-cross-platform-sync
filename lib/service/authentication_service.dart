de7b1008ae3c168e74e56066a326608b7cb2d6dc
cyliong <liongtheprogrammer@gmail.com>
2021-02-21 23:40:37 +0800

    Add the signUp() method to AuthenticationService

--- lib/service/authentication_service.dart
+++ lib/service/authentication_service.dart
@@ -3,5 +3,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthenticationService {
   final _firebaseAuth = FirebaseAuth.instance;
 
+  Future<String> signUp({String email, String password}) async {
+    final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return userCredential.user.uid;
+  }
+
   Future<void> signOut() => _firebaseAuth.signOut();
 }
