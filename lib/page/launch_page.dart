c437d9f24ae35395d91162a1bf33a862b80194f8
cyliong <liongtheprogrammer@gmail.com>
2021-02-25 23:02:22 +0800

    Create the LaunchPage skeleton

---
+++ lib/page/launch_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LaunchPage extends StatefulWidget {
+  @override
+  _LaunchPageState createState() => _LaunchPageState();
+}
+
+class _LaunchPageState extends State<LaunchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
