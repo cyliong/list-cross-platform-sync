cd6b3e1327a19f3dc6d39657b75fd12c53f54952
cyliong <liongtheprogrammer@gmail.com>
2021-03-12 15:47:10 +0800

    Refactor the AuthenticationService class to support null safety

--- lib/service/authentication_service.dart
+++ lib/service/authentication_service.dart
@@ -1,30 +1,30 @@
-// @dart=2.9
-
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenticationService {
   final _firebaseAuth = FirebaseAuth.instance;
 
-  String get currentUserId => _firebaseAuth.currentUser?.uid;
+  String? get currentUserId => _firebaseAuth.currentUser?.uid;
 
   static final _instance = AuthenticationService._();
   factory AuthenticationService() => _instance;
   AuthenticationService._();
 
-  Future<String> signUp({String email, String password}) async {
+  Future<String?> signUp(
+      {required String email, required String password}) async {
     final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
-    return userCredential.user.uid;
+    return userCredential.user?.uid;
   }
 
-  Future<String> signIn({String email, String password}) async {
+  Future<String?> signIn(
+      {required String email, required String password}) async {
     final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
-    return userCredential.user.uid;
+    return userCredential.user?.uid;
   }
 
   Future<void> signOut() => _firebaseAuth.signOut();
