196413e7b8285f1b79f87bc5d62ee45130b125f7
cyliong <liongtheprogrammer@gmail.com>
2021-09-27 21:05:03 +0800

    Provide a const constructor for LoadingPage

--- lib/page/loading_page.dart
+++ lib/page/loading_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:items/constants.dart';
 
 class LoadingPage extends StatelessWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
